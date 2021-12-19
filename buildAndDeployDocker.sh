fa00e87018e784c4de98c65d46b2638d1913e8be
Mathieu Grondin <mgrondin.mg@gmail.com>
2021-12-19 16:33:39 -0500

    clean slate

--- buildAndDeployDocker.sh
+++
@@ -1,4 +0,0 @@
-TAG="latest"
-docker build -t gcr.io/serene-foundry-234719/abobot:latest .
-# docker push gcr.io/serene-foundry-234719/abobot:latest
-# gcloud compute instances reset abobot --zone northamerica-northeast1-a
