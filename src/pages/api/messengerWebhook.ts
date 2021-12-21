375e66dbfdc28a570230afe1cad75e56a2b0f1b5
Mathieu Grondin <mgrondin.mg@gmail.com>
2021-12-20 19:34:19 -0500

    added messenger webhook

---
+++ src/pages/api/messengerWebhook.ts
@@ -0,0 +1,37 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
+
+const messengerWebhook: NextApiHandler = async (request, response) => {
+  switch (request.method) {
+    case 'POST': return handleMessengerPost(request, response)
+    case 'GET':
+    default:
+      return handleMessengerGet(request, response)
+  }
+}
+
+function handleMessengerGet(request: NextApiRequest, response: NextApiResponse) {
+  const { query } = request;
+  const { hub } = query;
+  if (query) {
+    // Parse the query params
+    const mode = request.query['hub.mode'];
+    const token = request.query['hub.verify_token'];
+    if (mode && token) {
+      if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
+        console.log('WEBHOOK_VERIFIED');
+        const challenge = request.query['hub.challenge'];
+        response.status(200).send(challenge);
+
+      }
+    }
+    console.log('messengerWebhook', query)
+    console.log('messengerWebhook', JSON.stringify(hub))
+  }
+  response.status(403).send('verify tokens do not match');
+}
+
+function handleMessengerPost(request: NextApiRequest, response: NextApiResponse) {
+  console.log('New message received', JSON.parse(request.body))
+  response.send('ok')
+}
+export default messengerWebhook
