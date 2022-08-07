067e7a649dfa67f21b56ff7f7e7d9e4c3c475e1a
Mathieu Grondin <mathieu.grondin@ubisoft.com>
2022-08-06 21:35:43 -0400

    fix build

--- src/pages/api/match.ts
+++ src/pages/api/match.ts
@@ -1,10 +1,9 @@
 import { NextApiHandler } from "next";
-import { ApiError } from "next/dist/server/api-utils";
 import { MatchController } from "../../app/controller/match";
 import { handleRequest } from "../../helpers/handleRequest";
 
 const matchApi : NextApiHandler = async (request, response) => handleRequest(response, async () => {
-    const result = await MatchController.handleRequest(request)
+    const result = await MatchController.handleRequest(request);
     response.status(200).json(result);
 });
 
