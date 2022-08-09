4a7988872c0201181b7e5fec9cd583c6f2f75162
Mathieu Grondin <mgrondin.mg@gmail.com>
2022-08-08 22:25:06 -0400

    17: Messenger handling refactor (#18)
    * 17: Messenger handling refactor
    
    * cleanus
    
    * more cleaning
    
    Co-authored-by: Mathieu Grondin <mathieu.grondin@ubisoft.com>

--- src/app/validator/matchValidator.ts
+++ src/app/validator/matchValidator.ts
@@ -5,26 +5,26 @@ import SeasonRepository, { Season } from "../repository/seasonRepository";
 export const A_TEAM_INDEX = 0;
 export const B_TEAM_INDEX = 1;
 
-export const InvalidTeamQueryParam_LenghtMustBeTwo = (length: number): ApiError => new ApiError(400, `Two team ids must be provided. Received: ${length}`);
-export const InvalidTeamQueryParam_MustNotBeNull = (): ApiError => new ApiError(400, 'Team identifiers must not be null');
-export const InvalidTeamQueryParam_MustNotBeEqual = (): ApiError => new ApiError(400, 'Team identifiers must not the same');
-export const InvalidTeamQueryParam_TeamNotPlayingThisSeason = (teamId: string, seasonId: string): ApiError => new ApiError(400, `The following team does not play in the ${seasonId} season: ${teamId}`);
+export const InvalidNewMatchQuery_LenghtMustBeTwo = (length: number): ApiError => new ApiError(400, `Two team ids must be provided. Received: ${length}`);
+export const InvalidNewMatchQuery_MustNotBeNull = (): ApiError => new ApiError(400, 'Team identifiers must not be null');
+export const InvalidNewMatchQuery_MustNotBeEqual = (): ApiError => new ApiError(400, 'Team identifiers must not the same');
+export const InvalidNewMatchQuery_TeamNotPlayingThisSeason = (teamId: string, seasonId: string): ApiError => new ApiError(400, `The following team does not play in the ${seasonId} season: ${teamId}`);
 
 export function validateNewMatchTeams(teamIds: string[], season: Season){
     if(teamIds.length != 2){
-        throw InvalidTeamQueryParam_LenghtMustBeTwo(teamIds.length);
+        throw InvalidNewMatchQuery_LenghtMustBeTwo(teamIds.length);
     }
     const teamA = teamIds[A_TEAM_INDEX];
     const teamB = teamIds[B_TEAM_INDEX];
     if(teamA == null || teamB == null){
-        throw InvalidTeamQueryParam_MustNotBeNull();
+        throw InvalidNewMatchQuery_MustNotBeNull();
     }
     if(teamA == teamB){
-        throw InvalidTeamQueryParam_MustNotBeEqual();
+        throw InvalidNewMatchQuery_MustNotBeEqual();
     }
     teamIds.forEach((teamId) => {
         if(!season.teamIds.includes(teamId)){
-            throw InvalidTeamQueryParam_TeamNotPlayingThisSeason(teamId, season.id);
+            throw InvalidNewMatchQuery_TeamNotPlayingThisSeason(teamId, season.id);
         }
     })
 }
---
+++ src/pages/api/message.ts
@@ -0,0 +1,10 @@
+import { NextApiHandler } from "next";
+import { MessageController } from "../../app/controller/message";
+import { handleRequest } from "../../helpers/handleRequest";
+
+const matchApi : NextApiHandler = async (request, response) => handleRequest(response, async () => {
+    const result = await MessageController.handleRequest(request);
+    response.status(200).json(result);
+});
+
+export default matchApi;
