7daf84bc2177df630fd774b4822d28278220b3fd
Mathieu Grondin <mgrondin.mg@gmail.com>
2022-08-17 10:26:29 -0400

    validate that match does not exist before creating it (#25)
    Co-authored-by: Mathieu Grondin <mathieu.grondin@ubisoft.com>

--- src/app/validator/matchValidator.ts
+++ src/app/validator/matchValidator.ts
@@ -8,22 +8,30 @@ export const InvalidNewMatchQuery_LenghtMustBeTwo = (length: number): ApiError =
 export const InvalidNewMatchQuery_MustNotBeNull = (): ApiError => new ApiError(400, 'Team identifiers must not be null');
 export const InvalidNewMatchQuery_MustNotBeEqual = (): ApiError => new ApiError(400, 'Team identifiers must not the same');
 export const InvalidNewMatchQuery_TeamNotPlayingThisSeason = (teamId: string, seasonId: string): ApiError => new ApiError(400, `The following team does not play in the ${seasonId} season: ${teamId}`);
+export const InvalidNewMatchQuery_MatchAlreadyExists = (): ApiError => new ApiError(400, 'Match already exists');
 
-export function validateNewMatchTeams(teamIds: string[], season: Season){
+export function validateNewMatch(matchId: number, teamIds: string[], season: Season){
     if(teamIds.length != 2){
         throw InvalidNewMatchQuery_LenghtMustBeTwo(teamIds.length);
     }
+
     const teamA = teamIds[A_TEAM_INDEX];
     const teamB = teamIds[B_TEAM_INDEX];
     if(teamA == null || teamB == null){
         throw InvalidNewMatchQuery_MustNotBeNull();
     }
+
     if(teamA == teamB){
         throw InvalidNewMatchQuery_MustNotBeEqual();
     }
+
     teamIds.forEach((teamId) => {
         if(!season.teamIds.includes(teamId)){
             throw InvalidNewMatchQuery_TeamNotPlayingThisSeason(teamId, season.id);
         }
     });
+
+    if(season.matchIds.includes(`${matchId}`)){
+        throw InvalidNewMatchQuery_MatchAlreadyExists();
+    }
 }
