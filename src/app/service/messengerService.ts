c139f40b4d60b7c0769804fba2df977a92b29f01
Mathieu Grondin <mgrondin.mg@gmail.com>
2022-09-20 12:10:01 -0400

    Bring back vote (#36)
    * wip
    
    * use vote
    
    * fix rebase
    
    Co-authored-by: Mathieu Grondin <mathieu.grondin@ubisoft.com>

--- src/app/service/messengerService.ts
+++ src/app/service/messengerService.ts
@@ -11,26 +11,30 @@ function sendMessage(userId: string, message: string): Promise<void> {
         }
       };
 
-      // const url = `https://graph.facebook.com/v14.0/2222667838060764/messages?access_token=${process.env.ACCESS_TOKEN}`;
-      // const response = await fetch(url, { method: 'POST', body: JSON.stringify(body) });
-      const response = await fetch(
-        `https://graph.facebook.com/v14.0/me/messages?access_token=${process.env.ACCESS_TOKEN}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
+      const requestInit = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          messaging_type: 'RESPONSE',
+          recipient: {
+            id: userId,
           },
-          method: 'POST',
-          body: JSON.stringify({
-            messaging_type: 'RESPONSE',
-            recipient: {
-              id: userId,
-            },
-            message: {
-              text: message,
-            },
-          }),
+          message: {
+            text: message,
+          },
+        }),
+      }
+      const url = `https://graph.facebook.com/v14.0/me/messages?access_token=${process.env.ACCESS_TOKEN}`
+      try{
+        const response = await fetch(url, requestInit);
+        if(response.status != 200){
+          process.stdout.write(`Error sending message: ${response.status}`);
         }
-      );
+      } catch (e){
+        process.stdout.write(`Error sending message: ${e.message}`);
+      }
     });
 }
 
