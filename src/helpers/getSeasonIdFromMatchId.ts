56ec080aea3ff47ae22706730ad0ee72b917046b
Mathieu Grondin <mgrondin.mg@gmail.com>
2022-12-06 21:25:24 -0500

    53: Fix new match creation (#55)
    Co-authored-by: Mathieu Grondin <mathieu.grondin@ubisoft.com>

--- src/helpers/getSeasonIdFromMatchId.ts
+++ src/helpers/getSeasonIdFromMatchId.ts
@@ -1,7 +1,7 @@
 import { getMonthFromDate, getYearFromDate } from "./timeHelper";
 
 export function getSeasonIdFromMatchId(matchId: number): string{
-    var earlySeasonDateRange = [6, 11]
+    var earlySeasonDateRange = [6, 12]
     var year = parseInt(`${matchId}`.slice(0,4));
     var month = parseInt(`${matchId}`.slice(4,6));
     if(earlySeasonDateRange[0] <= month && earlySeasonDateRange[1] >= month){
