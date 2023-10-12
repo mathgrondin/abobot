4b573edbf15e8bc2ef9e3cc43e6e53fbb84cb5af
Mathieu Grondin <mgrondin.mg@gmail.com>
2023-10-11 21:28:54 -0400

    lint

--- src/pages/api/testMessage.ts
+++ src/pages/api/testMessage.ts
@@ -3,8 +3,8 @@ import { MessageController } from '../../app/controller/message';
 import { handleRequest } from '../../helpers/handleRequest';
 
 const testMessageApi: NextApiHandler = async (request, response) => handleRequest(response, async () => {
-      const result = await MessageController.handleRequest(request);
-    response.status(200).json({result: JSON.stringify(result)});
+  const result = await MessageController.handleRequest(request);
+  response.status(200).json({result: JSON.stringify(result)});
 });
 
 export default testMessageApi;
